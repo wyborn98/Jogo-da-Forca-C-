b5ca6f3d7c86d44da7b6965dabd2f5c6963d3d32
wyborn98 <viniciuspalmacs@gmail.com>
2020-08-13 15:50:15 -0300

    Parcialmente finalizado

--- Project/include/my_math.h
+++
@@ -1,9 +0,0 @@
-#ifndef _MY_MATH_H
-#define _MY_MATH_H
-#include "my_math.cpp"
-
-int fatorial(int n);
-int area_quadrado(int lado);
-int area_retangulo(int altura, int base);
-
-#endif
--- Project/src/main.cpp
+++ Project/src/main.cpp
@@ -1,16 +1,70 @@
-#include <iostream>
-#include "../include/my_math.h"
-using namespace std;
+ // Solução do problema do Jogo da Forca baseada nas videoaulas da G-Tech Soluções.
+
+#include "../include/my_functions.h"
+
 
 int main()
 {   
-    int n = 5;
-    int a = 3;
-    int b = 2;
     
-    cout << "Fatorial de 5: " << fatorial(n) << endl;
-    cout << "Area do quadrado: com lado " << n << ": " << area_quadrado(n) << endl;
-    cout << "Area do retangulo com altura " << a << " e base " << b << ": " << area_retangulo(a, b)  << endl;
-    
-    return 0;   
+        //Inicializações necessárias
+        contador_letras = 0;
+        contador_erros  = 0;    
+        int linha_palavra = sorteia();
+        copia_palavra(linha_palavra);
+        limpar_campos();
+
+        while(contador_erros < 6 && palavra_completa() == false)
+        {   
+                        
+            system("clear");
+            imprimir_corpo(contador_erros);
+            cout << endl;
+            imprimir_palavra();
+            imprimir_digitadas(letras_digitadas, contador_letras);
+            cout << endl << "Tentativas restantes: " << 6 - contador_erros << endl << endl;
+            cout << endl << endl;
+            
+            cout << "Digite uma letra: ";
+            cin  >> letra;
+            letra = tolower(letra);
+
+            if (verifica_letra(letra, letras_digitadas)) 
+            {
+                cout << "Letra já foi digitada! Tente outra." << endl << endl;
+                getchar();
+            } else
+            {
+                letras_digitadas[contador_letras] = letra;
+                contador_letras++;
+
+                if(verifica_letra(letra, palavra_sorteada))
+                {                   
+                    substituir(letra);
+                }
+                else 
+                {   
+                    cout << "Letra incorreta! Tente outra." << endl;
+                    contador_erros++;
+                } 
+            }  
+        
+        }
+
+        if(contador_erros >= 6)
+        {
+            system("clear");
+            cout << "\nSuas tentativas acabaram! Você perdeu :(" << endl << endl;
+            getchar();
+            
+        }
+
+        if(palavra_completa() == true)
+        {   
+            system("clear");     
+            cout << "\n Parabens, voce venceu!!!" << endl << endl;            
+            
+        }        
+
+    return 0;
+    //Fim :)
 }
